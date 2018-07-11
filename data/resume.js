b923f083d6eb76396930e5a23b81f19eed50e5f0
Billy Ceskavich <billy.ceskavich@voxmedia.com>
2018-07-11 08:37:36 -0400

    Initial commit with Gatsby boilerplate + some personal stuffs

---
+++ data/resume.js
@@ -0,0 +1,117 @@
+module.exports = {
+  education: [
+    {
+      company: null,
+      title: 'M.S. Information Management',
+      location: 'Syracuse University',
+      date: 'Graduated Dec 2015',
+      bullets: ['Engagement Fellow']
+    },
+    {
+      company: null,
+      title: 'B.S. Information Management\nB.A. Political Science',
+      location: 'Syracuse University',
+      date: 'Graduated May 2014',
+      bullets: [
+        'Summe Cum Laude',
+        '[Remembrance Scholar](http://remembrance.syr.edu/)'
+      ]
+    }
+  ],
+  experience: [
+    {
+      company: '[Vox Media](http://product.voxmedia.com/)',
+      title: 'Full Stack Engineer',
+      location: 'New York City',
+      date: 'February 2016 - Present',
+      bullets: [
+        'Building Anthem, a collaborative rich text editor for Vox Media’s \
+        Chorus publishing platform',
+        'Contribute code to our custom autosave pipeline using \
+        [rich-text](https://github.com/ottypes/rich-text) and operational \
+        transformations to power multi-user writing experiences',
+        'Build for a highly interactive web application using \
+        component based frameworks like Ractive and React',
+        'Write and maintain APIs for both NodeJS and Ruby on Rails environments'
+      ]
+    },
+    {
+      company: '[Made by Many](http://www.madebymany.com/)',
+      title: 'Product Engineering Intern',
+      location: 'New York City',
+      date: 'May 2015 - August 2015',
+      bullets: [
+        'Built [HelloBacker](http://www.hellobacker.com/), an application for \
+        matching event planners and sponsors, as part of an intern product \
+        team tasked with creating a production-ready application from scratch \
+        in twelve weeks',
+        'Served as the lead developer and participated in the full product \
+        development process, including research, prototyping, and build',
+        'Developed HelloBacker in a full-stack Javascript environment, working \
+        with React and Flux on the front-end and Express and Bookshelf on the \
+        back-end'
+      ]
+    },
+    {
+      company: '[BITS Lab](http://bits.ischool.syr.edu/)',
+      title: 'Developer',
+      location: 'Syracuse, NY',
+      date: 'September 2014 - December 2015',
+      bullets: [
+        'Lead developer of [STACKS](https://github.com/bitslabsyr/stack), a \
+        toolkit written in Python for researchers to collect and analyze large \
+        social media data sets from Twitter’s Streaming API and Facebook’s \
+        Graph API',
+        'Contracted by the World Bank to design and implement custom data \
+        collection processes',
+        'Collaborated with research team to define goals, analyze social data, \
+        and produce visualizations'
+      ]
+    },
+    {
+      company: '[Twitter](https://about.twitter.com/)',
+      title: 'Technical Program Manager Intern',
+      location: 'San Francisco, CA',
+      date: 'May 2014 - August 2014',
+      bullets: [
+        'Lead cross-functional teams in Twitter’s Consumer Product Engineering \
+        group to deliver strategic projects and programs in an Agile \
+        environment',
+        'Shipped two iterative feature improvements to Twitter Search for \
+        logged-out web users',
+        'Established a program foundation for the restructuring of Twitter’s \
+        experimentation framework',
+        'Interfaced with Twitter’s custom Scala stack and front-end framework'
+      ]
+    },
+    {
+      company: '[Hack Upstate](http://www.hackupstate.com/)',
+      title: 'Fellow',
+      location: 'Syracuse, NY',
+      date: 'June 2014 - May 2015',
+      bullets: [
+        'Organize Hack Upstate’s biannual hackathon in Syracuse, New York',
+        'Lead partner relations with AT&T, collaborating to produce four \
+        virtual hackathons throughout the upstate New York region to date',
+        'Lead developer of Hack Upstate’s digital content and web presence',
+        'Provide technical support to partners and attendees at all events'
+      ]
+    },
+    {
+      company: '[Evernote](https://evernote.com/)',
+      title: 'Developer Relations Intern',
+      location: 'Redwood City, CA',
+      date: 'May 2013 - August 2013',
+      bullets: [
+        'Served as a project lead for the Evernote Devcup, crafting \
+        communications and digital content for an international developer \
+        competition with 570 participants',
+        'Coordinated four developer events in the San Francisco Bay Area to \
+        promote the Evernote API',
+        'Developed and implemented components of the Evernote Accelerator \
+        incubator program',
+        'Implemented curriculum updates to the Evernote Developers homepage'
+      ]
+    }
+  ]
+}
