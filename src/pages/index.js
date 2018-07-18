23ca4f07ddb93123e642d9dc800a76e1070e82b8
Billy Ceskavich <billy.ceskavich@voxmedia.com>
2018-07-18 09:03:45 -0400

    The homepage can be a functional component again

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,57 +25,53 @@ const LinksList = styled.ul`
   }
 `;
 
-export default class Index extends Component {
-  render() {
-    return (
-      <div>
-        <Title maxSize="50" minSize="30">
-          <TitleText>Billy Ceskavich</TitleText>
-        </Title>
-        <AboutChunk>
-          I write software for the web. I currently work for{' '}
-          <a href="https://product.voxmedia.com/pages/our-team">Vox Media</a> as
-          a full-stack engineer contributing to our publishing platform{' '}
-          <a href="https://getchorus.voxmedia.com/">Chorus</a>.
-        </AboutChunk>
-        <AboutChunk>
-          Previously, I was in school. Specifically, from 2010 – 2015 at the
-          Syracuse University <a href="https://ischool.syr.edu/">iSchool</a>.
-          During that time, I held engineering and product internships at{' '}
-          <a href="http://www.madebymany.com/">Made by Many</a>,{' '}
-          <a href="https://twitter.com/">Twitter</a>, and{' '}
-          <a href="https://evernote.com/">Evernote</a>.
-        </AboutChunk>
-        <AboutChunk>
-          You can look through a list of my current and past{' '}
-          <Link to="/projects">projects</Link>, which I update occasionally. You
-          can also get my <Link to="/resume">resume</Link>.
-        </AboutChunk>
-        <AboutChunk>
-          My email is{' '}
-          <em>
-            bceskavich <strong>[at]</strong> gmail <strong>[dot]</strong> com
-          </em>
-        </AboutChunk>
-        <AboutChunk>
-          My last name is pronounced <em>SES</em>-kah-vitch
-        </AboutChunk>
-        <AboutChunk>You can find me on your favorite websites:</AboutChunk>
-        <LinksList>
-          <li>
-            <a href="https://twitter.com/ceskavich">Twitter</a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/ceskavich/">Instagram</a>
-          </li>
-          <li>
-            <a href="https://github.com/bceskavich">GitHub</a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/ceskavich/">LinkedIn</a>
-          </li>
-        </LinksList>
-      </div>
-    );
-  }
-}
+export default () => (
+  <div>
+    <Title maxSize="50" minSize="30">
+      <TitleText>Billy Ceskavich</TitleText>
+    </Title>
+    <AboutChunk>
+      I write software for the web. I currently work for{' '}
+      <a href="https://product.voxmedia.com/pages/our-team">Vox Media</a> as a
+      full-stack engineer contributing to our publishing platform{' '}
+      <a href="https://getchorus.voxmedia.com/">Chorus</a>.
+    </AboutChunk>
+    <AboutChunk>
+      Previously, I was in school. Specifically, from 2010 – 2015 at the
+      Syracuse University <a href="https://ischool.syr.edu/">iSchool</a>. During
+      that time, I held engineering and product internships at{' '}
+      <a href="http://www.madebymany.com/">Made by Many</a>,{' '}
+      <a href="https://twitter.com/">Twitter</a>, and{' '}
+      <a href="https://evernote.com/">Evernote</a>.
+    </AboutChunk>
+    <AboutChunk>
+      You can look through a list of my current and past{' '}
+      <Link to="/projects">projects</Link>, which I update occasionally. You can
+      also get my <Link to="/resume">resume</Link>.
+    </AboutChunk>
+    <AboutChunk>
+      My email is{' '}
+      <em>
+        bceskavich <strong>[at]</strong> gmail <strong>[dot]</strong> com
+      </em>
+    </AboutChunk>
+    <AboutChunk>
+      My last name is pronounced <em>SES</em>-kah-vitch
+    </AboutChunk>
+    <AboutChunk>You can find me on your favorite websites:</AboutChunk>
+    <LinksList>
+      <li>
+        <a href="https://twitter.com/ceskavich">Twitter</a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/ceskavich/">Instagram</a>
+      </li>
+      <li>
+        <a href="https://github.com/bceskavich">GitHub</a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/ceskavich/">LinkedIn</a>
+      </li>
+    </LinksList>
+  </div>
+);
