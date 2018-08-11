dba1184479a411003aac63f4054ef94b890b87ee
Billy Ceskavich <billy.ceskavich@voxmedia.com>
2018-08-11 09:15:46 -0400

    Add gatsby-plugin-react-next to use latest React 16.x ahead of Gatsby v2 post-beta

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-next',
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
